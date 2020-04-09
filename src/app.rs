eed8899e96475373433950f36755a7a3c3c92b9a
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-04-08 18:02:14 -0700

    Migrates recognition view functions into the view module.

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use yew::{Component, ComponentLink, html, Html, ShouldRender};
+use yew::{Component, ComponentLink, Html, ShouldRender};
 
 use super::view;
 
@@ -19,7 +19,7 @@ impl Component for Model {
 		let new_state = match msg {
 			Msg::Quit => Some(State::Idle),
 			Msg::Recognition => {
-				let game = RecognitionGame { active_verb: active_verb(), show_answer: false, remaining: 4 };
+				let game = RecognitionGame { active_verb: any_verb(), show_answer: false, remaining: 4 };
 				Some(State::Recognition { game })
 			}
 			Msg::ShowAnswer => match &self.state {
@@ -58,7 +58,7 @@ impl Component for Model {
 	fn view(&self) -> Html {
 		match &self.state {
 			State::Idle => view::idle(&self.link),
-			State::Recognition { game } => self.view_recognition(game),
+			State::Recognition { game } => view::recognition_page(game, &self.link),
 		}
 	}
 }
@@ -77,80 +77,11 @@ pub enum Msg {
 	Pass,
 }
 
-impl Model {
-	fn view_answer_text(&self) -> Html {
-		html! {
-			<div>
-			<p>{ "＝ can forget" }</p>
-			</div>
-		}
-	}
-
-	fn view_answer(&self, show_answer: bool) -> Html {
-		if show_answer {
-			self.view_answer_text()
-		} else {
-			view::answer_button(&self.link)
-		}
-	}
-
-	fn view_next(&self) -> Html {
-		html! {
-			<div>
-			<p>
-			<button onclick=self.link.callback(|_| Msg::Repeat)>{ "Hard. Repeat" }</button>
-			</p>
-			<p>
-			<button onclick=self.link.callback(|_| Msg::Pass)>{ "Easy. Next" }</button>
-			</p>
-			</div>
-		}
-	}
-
-	fn view_recognition(&self, game: &RecognitionGame) -> Html {
-		let RecognitionGame { active_verb, show_answer, remaining } = game;
-		html! {
-			<div>
-
-			<h1>
-			{"Recognition"}
-			<button onclick=self.link.callback(|_| Msg::Quit)>{ format!("Quit ({} remaining)", remaining+1) }</button>
-			</h1>
-			<div>
-			</div>
-			<p>
-			<audio controls=true src=audio_url(&active_verb.search)></audio>
-			</p>
-			<p>
-			{ self.view_answer(*show_answer) }
-			</p>
-			{
-				if *show_answer {
-					self.view_next()
-				} else {
-					html!{}
-				}
-			}
-
-            </div>
-        }
-	}
-}
-
-fn audio_url(search: &str) -> String {
-	format!("http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q={}&tl=ja", search)
-}
-
-fn active_verb() -> Verb {
-	let active_verb = Verb { search: "wasureraremasu".to_string() };
-	active_verb
-}
-
 #[derive(Clone)]
 pub struct RecognitionGame {
-	active_verb: Verb,
-	show_answer: bool,
-	remaining: usize,
+	pub active_verb: Verb,
+	pub show_answer: bool,
+	pub remaining: usize,
 }
 
 impl RecognitionGame {
@@ -164,7 +95,7 @@ impl RecognitionGame {
 		} else {
 			Some(RecognitionGame {
 				show_answer: false,
-				active_verb: active_verb(),
+				active_verb: any_verb(),
 				remaining: self.remaining - 1,
 			})
 		}
@@ -173,7 +104,7 @@ impl RecognitionGame {
 	fn repeat_question(&self) -> Self {
 		RecognitionGame {
 			show_answer: false,
-			active_verb: active_verb(),
+			active_verb: any_verb(),
 			remaining: self.remaining,
 		}
 	}
@@ -181,5 +112,9 @@ impl RecognitionGame {
 
 #[derive(Clone)]
 pub struct Verb {
-	search: String
+	pub search: String
+}
+
+pub fn any_verb() -> Verb {
+	Verb { search: "wasureraremasu".to_string() }
 }
--- src/view.rs
+++ src/view.rs
@@ -1,6 +1,6 @@
 use yew::{ComponentLink, html, Html};
 
-use crate::app::{Model, Msg};
+use crate::app::{Model, Msg, RecognitionGame};
 
 pub fn answer_button(link: &ComponentLink<Model>) -> Html {
 	html! {
@@ -8,14 +8,90 @@ pub fn answer_button(link: &ComponentLink<Model>) -> Html {
 	}
 }
 
-pub fn flat_button(label: &str, msg: Msg, link: &ComponentLink<Model>) -> Html {
+
+pub fn idle(link: &ComponentLink<Model>) -> Html {
+	page("Verb Trainer", link, idle_content)
+}
+
+fn idle_content(link: &ComponentLink<Model>) -> Html {
 	html! {
-        <button class="mdl-button mdl-js-button" onclick=link.callback(move |_| msg)>{ label }</button>
+<ol>
+	<li>
+	{ flat_button( "Recognition Game", Msg::Recognition, link)}
+    </li>
+</ol>
+    }
+}
+
+
+fn recognition_answer() -> Html {
+	html! {
+			<div>
+			<p>{ "＝ can forget" }</p>
+			</div>
+		}
+}
+
+fn recognition_answer_row(show_answer: bool, link: &ComponentLink<Model>) -> Html {
+	if show_answer {
+		recognition_answer()
+	} else {
+		answer_button(link)
 	}
 }
 
-fn page(link: &ComponentLink<Model>, content: impl Fn(&ComponentLink<Model>) -> Html) -> Html {
-	let title = "Verb Trainer";
+fn recognition_action_row(link: &ComponentLink<Model>) -> Html {
+	html! {
+			<div>
+			<p>
+			<button onclick=link.callback(|_| Msg::Repeat)>{ "Hard. Repeat" }</button>
+			</p>
+			<p>
+			<button onclick=link.callback(|_| Msg::Pass)>{ "Easy. Next" }</button>
+			</p>
+			</div>
+		}
+}
+
+pub fn recognition_page(game: &RecognitionGame, link: &ComponentLink<Model>) -> Html {
+	let RecognitionGame { active_verb, show_answer, remaining } = game;
+	html! {
+			<div>
+
+			<h1>
+			{"Recognition"}
+			<button onclick=link.callback(|_| Msg::Quit)>{ format!("Quit ({} remaining)", remaining+1) }</button>
+			</h1>
+			<div>
+			</div>
+			<p>
+			<audio controls=true src=audio_url(&active_verb.search)></audio>
+			</p>
+			<p>
+			{ recognition_answer_row(*show_answer, link) }
+			</p>
+			{
+				if *show_answer {
+					recognition_action_row(link)
+				} else {
+					html!{}
+				}
+			}
+            </div>
+        }
+}
+
+fn audio_url(search: &str) -> String {
+	format!("http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q={}&tl=ja", search)
+}
+
+pub fn flat_button(label: &str, msg: Msg, link: &ComponentLink<Model>) -> Html {
+	html! {
+        <button class="mdl-button mdl-js-button mdl-button--primary" onclick=link.callback(move |_| msg)>{ label }</button>
+	}
+}
+
+pub fn page(title: &str, link: &ComponentLink<Model>, content: impl Fn(&ComponentLink<Model>) -> Html) -> Html {
 	let link_text = "Link";
 	html! {
 <div class="mdl-layout mdl-js-layout">
@@ -37,18 +113,4 @@ fn page(link: &ComponentLink<Model>, content: impl Fn(&ComponentLink<Model>) -> 
   <main class="mdl-layout__content">{content(link)}</main>
 </div>
 	}
-}
-
-pub fn idle(link: &ComponentLink<Model>) -> Html {
-	page(link, idle_content)
-}
-
-fn idle_content(link: &ComponentLink<Model>) -> Html {
-	html! {
-<ol>
-	<li>
-	{ flat_button( "Recognition Game", Msg::Recognition, link)}
-    </li>
-</ol>
-    }
 }

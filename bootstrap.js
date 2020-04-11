b5caeb3885ddae7566203722eeeb35e9630e25c1
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-04-11 15:52:23 -0700

    Adjust import parameter.

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,7 @@
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
-import("./pkg")
+import("./pkg/potential.js")
     .then(module => {
         module.run_app();
         componentHandler.upgradeDom();

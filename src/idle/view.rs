fe34d5093a5d7a55b21cfb7ec8e5a592670e0922
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-04-15 20:16:44 -0700

    Updates home page.

--- src/idle/view.rs
+++ src/idle/view.rs
@@ -9,14 +9,25 @@ pub fn page(link: &ComponentLink<Model>) -> Html {
 
 fn content(link: &ComponentLink<Model>, _: &()) -> Html {
 	html! {
-		<div class="mdl-grid center-items">
-			<div class="mdl-cell mdl-cell--4-col mdl-cell--middle">
-		        <button
-		            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
-		            style="width:100%"
-		            onclick=link.callback(|_| Msg::Recognition)>
-		            { "Recognition Challenge" }
-		        </button>
+		<div class="mdl-grid">
+			<div class="mdl-cell mdl-cell--4-col">
+				<div class=" mdl-card mdl-shadow--2dp">
+					<div class="mdl-card__title mdl-color--primary-dark">
+					    <h2 class="mdl-card__title-text" style="color:#fff">{"Recognition"}</h2>
+					</div>
+					<div class="mdl-card__title mdl-color--primary-dark">
+						<div class="mdl-card__subtitle-text"></div>
+					</div>
+					<div class="mdl-card__supporting-text">
+						{"Listen to and translate 20 verbs selected at random from chapters 1-17."}
+					</div>
+					<div class="mdl-card__actions mdl-card--border">
+						<button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+						    onclick=link.callback(|_| Msg::Recognition)>
+							{"Get Started"}
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
     }
